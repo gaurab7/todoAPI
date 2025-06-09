a2db7860efccb6a4705bbcadb6592daa81c47896
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-09 06:52:02 +0000

    updated server.js

--- src/server.js
+++ src/server.js
@@ -5,9 +5,33 @@ const app = express()
 //checks if .env has a PORT defined to use it, if no exist uses 8000
 const PORT = process.env.PORT || 8000
 
-//app.get('/', (req, res) => {
+//user json
+const users = [
+  { id: 1, username: '', passwordHash: '' } 
+];
+const todos = [
+    { id: 1, title: 'Buy groceries', completed: false },
+    { id: 2, title: 'Walk the dog', completed: true }
+  ];
+
+//enables to accept json and intepret it
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.redirect('/login');
+});
+app.get('/login', (req, res) => {
  
-//})
+})
+
+
+
+app.post('/login', (req, res) => {
+
+  const {un, pw} = req.body
+  res.send(`${un}, ${pw}`)
+})
+
 //starts server & listens for incomming HTTP requests on specified port
 app.listen(PORT, () => {
     console.log(`Server started on port: ${PORT}`)
