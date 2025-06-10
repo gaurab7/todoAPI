d1588ee8e1fb9944aaaf77a72741605a86779842
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-10 07:21:50 +0000

    modified authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,10 +6,12 @@ import db from '../config/database.js'
 const router = express.Router()
 
 router.post('/register', (req, res) => {
-
+    const { username, password } = req.body
+    console.log(username, password)
+    res.sendStatus(201)
 })
 router.post('/login', (req, res) => {
-    
+    const { username, password } = req.body
 })
 
 export default router
