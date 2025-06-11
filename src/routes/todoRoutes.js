60c9d11c01055fcb24fcf938b552bb2455bc8c9e
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-11 12:22:35 +0000

    added create new tasks

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -4,7 +4,7 @@ import db from '../config/database.js'
 const router = express.Router()
  
 //gets todo data for logged user
-router.get('/', (req, res) =>{
+router.get('/todos', (req, res) =>{
     const tasks = db.prepare(`SELECT * FROM todos WHERE userid = ?`)
     //.get returns the first element only, .all returns all elements of the array
     //here the midleware authenticates the user and provides the req
@@ -13,9 +13,10 @@ router.get('/', (req, res) =>{
    
 })
 //create new tasks
-router.post('/', (req, res) =>{
-    const newtask = db.prepare(`INSERT INTO todos(id, userid, task) VALUES(?, ?, ?)`)
-    const taskAdded = newtask.run
+router.post('/todos', (req, res) =>{
+    const { userid, task } = req.body
+    const newtask = db.prepare(`INSERT INTO todos( userid, task) VALUES( ?, ?)`)
+    const taskAdded = newtask.run(userid, task)
 })
 //update task
 //'/:id' to specify which task to update 
