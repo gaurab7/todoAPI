ed0c53d7dafcc40cff93c835aaab8549585b8e8a
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-11 13:40:20 +0000

    completed todo api endpoints

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -14,18 +14,34 @@ router.get('/todos', (req, res) =>{
 })
 //create new tasks
 router.post('/todos', (req, res) =>{
-    const { userid, task } = req.body
+    const { task } = req.body
     const newtask = db.prepare(`INSERT INTO todos( userid, task) VALUES( ?, ?)`)
-    const taskAdded = newtask.run(userid, task)
+    //rather than using { userid, task} = req.body we can get userid from the request itself
+    const taskAdded = newtask.run(req.userid, task)
+  
+    res.json({ id: taskAdded.lastInsertRowid, task, completed: 0 })
 })
 //update task
 //'/:id' to specify which task to update 
-router.put('/:id', (req, res) => {
+router.put('/todos/:id', (req, res) => {
+
+    const { state } = req.body
+    //params--> '/:id' here id is the param(after :)
+    //for queries--> '/:id?q="fksj"' here fksj is the query and we use req.query for that
+    const { id } = req.params 
+    const update = db.prepare('UPDATE todos SET state = ? WHERE id = ?')
+    update.run(state, id)
+    res.json({ message: "Todo completed" })
 
 })
 //delete task
-router.delete('/:id', (req, res) => {
-
+router.delete('/todos/:id', (req, res) => {
+     const { id } = req.params
+     const userId = req.userid
+     const del = db.prepare(`DELETE FROM todos WHERE id = ? AND userid = ?`)
+     //filtering by id and userid so that a user doesnt delete others tasks
+     del.run(id, userId)
+     res.json({msg: "Todo Deleted"})
 })
 
 export default router
