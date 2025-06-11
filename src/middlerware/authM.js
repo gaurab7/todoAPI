c1c86c444c65a4e869d450c20e170d71bfa23ea5
gaurab7 <96043792+gaurab7@users.noreply.github.com>
2025-06-11 12:20:54 +0000

    solved the invalid token issue

--- src/middlerware/authM.js
+++ src/middlerware/authM.js
@@ -1,14 +1,22 @@
 import jwt from 'jsonwebtoken'
 
 //middleware here intercepst the nw requests & verify that the token is of correct user
-function authMiddleware(req, res, next)
+function authM(req, res, next)
 {
-    const token = req.headers['authorization']
+    //this gets the token from the header => 'authorixation'
+    const authHeader = req.headers['authorization'];
+
+    //the token is in : Bearer <token>, this extracts the Bearer and provides just the token
+    const token = authHeader && authHeader.split(' ')[1];
+
     if(!token) {return res.status(401).json({msg: 'No Token'})}
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=> {
-        if(err) { return res.status(401).json({msg: 'Invalid Token'})}
+        if(err) { 
+            console.log(token) 
+            console.log(err)
+            return res.status(401).json({msg: 'Invalid Token'})}
         req.userid = decoded.id
         next()
     })
 }
-export default authMiddleware
+export default authM
